7792ec1b3609a0b1c23e2597db2b74811c0f8c33
WL <WL@WLtekiMacBook-Pro.local>
2013-10-04 01:36:36 +0800

    Initial Commit

---
+++ UDCSiOSApp4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UDCSiOSApp4
+//
+//  Created by WL on 13-10-4.
+//  Copyright (c) 2013年 WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UDCSiOSApp4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UDCSiOSApp4
+//
+//  Created by WL on 13-10-4.
+//  Copyright (c) 2013年 WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
